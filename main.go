19995418d3377a2a0a74884290dd425e64ac0c8c
Dzianis Lapanik <dzianis_lapanik@epam.com>
2022-02-17 11:40:10 +0300

    GetMessage func created

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 	"github.com/kyokomi/emoji/v2"
 )
 
+func GetMessage() string {
+	return emoji.Sprint("Hello ", ":world_map:!")
+}
+
 func main() {
-	fmt.Println(emoji.Sprint("Hello ", ":world_map:!"))
+	fmt.Println(GetMessage())
 }
